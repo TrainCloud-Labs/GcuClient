ea3be0689d635ddd34c89784eb51ef9cb8eba578
Ferenc2022 <nico@caratiola.net>
2024-07-05 21:15:18 +0200

    Update Program.cs

--- GcuClient/Program.cs
+++ GcuClient/Program.cs
@@ -1,13 +1,20 @@
-﻿using System.ServiceModel;
+﻿using GcuClient.Wdr.ServiceReference;
+using System.ServiceModel;
 
 Console.WriteLine("Hello, Gcu!");
 
 string userName = "1234"; // Format 1234 (API User, not your personal user)
-string password = "Top$ecret123!";
+string password = "Topsecret";
+
+string fullUic = "378058406646";
+string carAvvCode = "4133";
+string trainnumber = "47112";
+
+string reportId = "123456"; //Guid.NewGuid().ToString();
 
 await SendDamageProtocoll();
 
-await GetRsds();
+//await GetRsds();
 
 /// Sends a "Schadwagen-Protokoll"
 async Task SendDamageProtocoll()
@@ -39,26 +46,192 @@ async Task SendDamageProtocoll()
             {
                 MessageDateTime = DateTime.Now,
                 MessageIdentifier = messageIdentifier,
-                MessageType = "6004", // Unclear
-                MessageTypeVersion = "RSRDM0100" // Unclear
+                MessageType = "5502", // Unclear
+                MessageTypeVersion = "2.1.8" // Unclear
             },
-            SenderReference = senderReference
+            //SenderReference = userName//senderReference
         };
 
-        GcuClient.Wdr.ServiceReference.WagonDamageReportMessageContactUserRU contactUserRU = new();
-        GcuClient.Wdr.ServiceReference.TransportInformation transportInfo = new();
-        GcuClient.Wdr.ServiceReference.DamageDetection damageDetection = new();
-        GcuClient.Wdr.ServiceReference.ExistingLabels existingLabels = new();
-        GcuClient.Wdr.ServiceReference.NewLabels newLabels = new();
-        GcuClient.Wdr.ServiceReference.DamageDescription damageDescription = new();
+        GcuClient.Wdr.ServiceReference.AdministrativeContactInformation administrativeContactInformation = new()
+        {
+            Name = "Mickmouse GmbH",
+            eMail = "info@test.de",
+            FreeTextField = "Micky Mouse Street, 666125 Musterhausen",
+        };
+
+        GcuClient.Wdr.ServiceReference.WagonDamageReportMessageContactUserRU contactUserRU = new()
+        {
+            AdministrativeContactInformation = administrativeContactInformation,
+        };
+
+        GcuClient.Wdr.ServiceReference.ConsignmentIdent consignmentIdent = new()
+        {
+            Value = "AB 123 456 789", //Sendungsnummer
+
+        };
+
+        GcuClient.Wdr.ServiceReference.CountryCodeISO originCountryCodeISO = new()
+        {
+            Value = "XX",
+        };
+        GcuClient.Wdr.ServiceReference.CountryCodeISO destinationCountryCodeISO = new()
+        {
+            Value = "FR",
+        };
+        GcuClient.Wdr.ServiceReference.CountryCodeISO preditorCountryCodeISO = new()
+        {
+            Value = "FR",
+        };
+
+        GcuClient.Wdr.ServiceReference.LocationSubsidiaryCode originLocationSubsidiaryCode = new()
+        {
+            Value = "42",
+        };
+
+
+
+        GcuClient.Wdr.ServiceReference.LocationSubsidiaryIdentification originLocationSubsidiaryIdentification = new()
+        {
+            LocationSubsidiaryCode = originLocationSubsidiaryCode,
+        };
+
+        GcuClient.Wdr.ServiceReference.TrainIdentifier trainIdentifier = new()
+        {
+            Item = trainnumber,
+        };
+        GcuClient.Wdr.ServiceReference.CompositIdentifierPlannedType trainIdentifier3 = new()
+        {
+            StartDate = DateTime.Now,
+        };
+
+        GcuClient.Wdr.ServiceReference.TransportOperationalIdentifiers transportOperationalIdentifiers = new()
+        {
+            StartDate = DateTime.UtcNow,
+            TimetableYear = "2024",
+            Company = "TMPC",
+            Variant = "545",
+        };
+
+        GcuClient.Wdr.ServiceReference.LocationIdent origin = new()
+        {
+            LocationPrimaryCode = "99999",
+            PrimaryLocationName = "Frankfurt",
+            //CountryCodeISO = originCountryCodeISO,
+            //LocationSubsidiaryIdentification = originLocationSubsidiaryIdentification,
+        };
+
+
+
+        GcuClient.Wdr.ServiceReference.LocationIdent destination = new()
+        {
+            LocationPrimaryCode = "99999",
+            PrimaryLocationName = "Idstein",
+            //CountryCodeISO = destinationCountryCodeISO,
+        };
+
+        GcuClient.Wdr.ServiceReference.LocationIdent locationDamageDetection = new()
+        {
+            LocationPrimaryCode = "99999",
+            PrimaryLocationName = "Niedernhausen",
+            //CountryCodeISO = destinationCountryCodeISO,
+        };
+        GcuClient.Wdr.ServiceReference.HandingOverCompany handingOverCompany = new()
+        {
+            Item = "DB Cargo",
+            ItemElementName = ItemChoiceType.CompanyName,
+            CompanyType = HandingOverCompanyCompanyType.NonGCUsignatoryRU,
+            CompanyTypeSpecified = true,
+        };
+
+
+
+        GcuClient.Wdr.ServiceReference.TransportInformation transportInfo = new()
+        {
+            ConsignmentNumber = consignmentIdent,
+            DepartureDate = System.DateTime.Today,
+            LoadingStatus = LoadingStatus.Item1,
+            Destination = destination,
+            TrainIdentifier = trainIdentifier,
+            Origin = origin,
+        };
+
+
+        GcuClient.Wdr.ServiceReference.DamageDetection damageDetection = new()
+        {
+            Location = locationDamageDetection,
+            DetectedUponHandover = false,
+            DetectionDate = System.DateTime.Today,
+            HandingOverCompany = handingOverCompany,
+        };
+
+        GcuClient.Wdr.ServiceReference.ExistingLabelsReportingRU existingLabelsReportingRU = new()
+        {
+            Item = "DB Cargo",
+            ItemElementName = ItemChoiceType1.CompanyName,
+        };
+
+        GcuClient.Wdr.ServiceReference.ExistingLabels existingLabels = new()
+        {
+            DamageLabel = [LabelType.K],
+            DateSpecified = true,
+            Date = System.DateTime.Today,
+            ReportingRU = existingLabelsReportingRU,
+        };
+        GcuClient.Wdr.ServiceReference.NewLabels newLabels = new()
+        {
+            DamageLabel = [LabelType.R1],
+            WagonDetached = false,
+            DispatchedToWorkshop = NewLabelsDispatchedToWorkshop.AfterUnloading,
+        };
+        GcuClient.Wdr.ServiceReference.AddressInformation adressInfomationPerpetrator = new()
+        {
+            Address = "Musterstraße 36",
+            CityTown = "Musterhausen",
+            PostalCode = "65894",
+            CompanyName = "BRD GmbH",
+            CountryCodeISO = preditorCountryCodeISO,
+        };
+
+        GcuClient.Wdr.ServiceReference.DamageDescriptionCategoryPerpetrator damageDescriptionCategoryPerpetrator = new()
+        {
+            Remarks = "this is a free text",
+            AddressInformation = adressInfomationPerpetrator,
+            AdministrativeContactInformation = administrativeContactInformation,
+
+
+
+        };
+        GcuClient.Wdr.ServiceReference.DamageDescriptionCategory damageDescriptionCategory = new()
+        {
+            DamageCategory = DamageDescriptionCategoryDamageCategory.Wear,
+            Perpetrator = damageDescriptionCategoryPerpetrator,
+
+        };
+        GcuClient.Wdr.ServiceReference.Damage codeDigits = new()
+        {
+            CodeDigit1 = "1",
+            CodeDigit2 = "2",
+            CodeDigit3 = "3",
+            CodeDigit4 = "4",
+            OldDamage = false,
+            OldDamageSpecified = false,
+        };
+        GcuClient.Wdr.ServiceReference.DamageDescription damageDescription = new()
+        {
+            Remarks = "free Stuff as Comment",
+            Category = damageDescriptionCategory,
+            Damage = [codeDigits]
+
+        };
+
         bool attachments = false;
 
         GcuClient.Wdr.ServiceReference.wdrRequest request = new(msgHeader,
-                                                                "reportId",
-                                                                "UserRU",
+                                                                reportId,
+                                                                userName,
                                                                 contactUserRU,
-                                                                "Keeper",
-                                                                "WagonNumberFreight",
+                                                                carAvvCode,
+                                                                fullUic,
                                                                 transportInfo,
                                                                 damageDetection,
                                                                 existingLabels,
@@ -66,7 +239,7 @@ async Task SendDamageProtocoll()
                                                                 damageDescription,
                                                                 attachments);
 
-        EndpointAddress ea = new("https://prod.gcubroker.org/wdr"); // https://stage.gcubroker.org/wdr
+        EndpointAddress ea = new("https://stage.gcubroker.org/wdr"); // https://prod.gcubroker.org/wdr
 
         GcuClient.Wdr.ServiceReference.WdrEndpointClient client = new(httpBinding, ea);
         client.ClientCredentials.UserName.UserName = userName;
@@ -77,6 +250,8 @@ async Task SendDamageProtocoll()
         /*
          * Work with the resonse from here.
          */
+
+        string Test = "Test";
     }
     catch (Exception ex)
     {
